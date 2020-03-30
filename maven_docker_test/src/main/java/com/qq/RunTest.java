def58b03108266ee208be84482e21c0915e4a667
dxnew <unreallerz@gmail.com>
2020-03-30 13:49:05 +0300

    test updated

--- maven_docker_test/src/main/java/com/qq/RunTest.java
+++ maven_docker_test/src/main/java/com/qq/RunTest.java
@@ -1,5 +1,6 @@
 package com.qq;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -8,5 +9,6 @@ public class RunTest {
     @Test
     public void test() {
         Run run = new Run("Test-Test-Test");
+        Assert.fail("&&&Fail&&&");
     }
 }
